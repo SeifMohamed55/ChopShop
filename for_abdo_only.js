62be5fc4f95d55669c1c9f85e090fef0f1769778
diahria <bodi.khaled24@gmail.com>
2023-05-05 22:53:00 +0300

    version 6.4

---
+++ for_abdo_only.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var x = document.getElementById("x");
+    var y = document.getElementById("y");
+    var z = document.getElementById("z");
+    var l = document.getElementsByClassName("btn1");
+}
