a8a5c5d730f77936041aa0aed80750e9cb5878dd
diahria <bodi.khaled24@gmail.com>
2023-05-07 21:50:50 +0300

    v 9.1

--- for_abdo_only.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-    var x = document.getElementById("x");
-    var y = document.getElementById("y");
-    var z = document.getElementById("z");
-    var l = document.getElementsByClassName("btn1");
-}
