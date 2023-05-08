96f971a6f887c40355d1041b965344210b488d46
diahria <bodi.khaled24@gmail.com>
2023-05-08 15:35:20 +0300

    ver 10.5(payment website)

--- for_abdo_only.js
+++ for_abdo_only.js
@@ -1,9 +1,26 @@
-<<<<<<< HEAD
-=======
-function myFunction() {
-    var x = document.getElementById("x");
-    var y = document.getElementById("y");
-    var z = document.getElementById("z");
-    var l = document.getElementsByClassName("btn1");
+var s =document.getElementById("change_chart");
+s.addEventListener("change", myFunction);
+function myFunction(event) {
+    var y =document.getElementById("creditcard");
+    var x =document.getElementById("amount");
+    var l =document.getElementById("q");
+    var u =document.getElementById("submit");
+    if(s.value == '1'){
+        y.style.display = "none";
+        x.style.display = "none";
+        l.style.display = "none";
+        u.style.display = "none";
+    }
+    else if (s.value == '2'){
+        y.style.display = "none";
+        x.style.display = "inline-block";
+        l.style.display = "inline-block";
+        u.style.display = "block";
+    }
+    else if (s.value == '3'){
+        y.style.display = "inline-block";
+        x.style.display = "none";
+        l.style.display = "inline";
+        u.style.display = "block";
+    }
 }
->>>>>>> db3b0b4851292bddbb91a8cc4ee0fdca778302ad
